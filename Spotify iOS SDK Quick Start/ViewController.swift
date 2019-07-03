3088943f2da59c1e957fa28c04c84732af8ef7af
Johanny A. Mateo <24462857+johannymateo@users.noreply.github.com>
2019-07-03 04:55:23 -0400

    Initial Commit

---
+++ Spotify iOS SDK Quick Start/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify iOS SDK Quick Start
+//
+//  Created by Johanny Mateo on 7/3/19.
+//  Copyright © 2019 Johanny A. Mateo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Spotify iOS SDK Quick StartTests/Spotify_iOS_SDK_Quick_StartTests.swift
@@ -0,0 +1,34 @@
+//
+//  Spotify_iOS_SDK_Quick_StartTests.swift
+//  Spotify iOS SDK Quick StartTests
+//
+//  Created by Johanny Mateo on 7/3/19.
+//  Copyright © 2019 Johanny A. Mateo. All rights reserved.
+//
+
+import XCTest
+@testable import Spotify_iOS_SDK_Quick_Start
+
+class Spotify_iOS_SDK_Quick_StartTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
